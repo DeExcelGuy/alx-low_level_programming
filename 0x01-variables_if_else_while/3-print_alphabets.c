d3731a0071084357b80217174148f2ee3e866f1a
DeExcelGuy <nreleattobra@gmail.com>
2022-06-03 16:39:08 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - is an entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		putchar(k);
+	}
+
+	for (k = 'A'; k <= 'Z'; k++)
+	{
+		putchar(k);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

9be5d7eaddaa4606845b517619d967b132bab0ab
DeExcelGuy <nreleattobra@gmail.com>
2022-06-03 17:08:25 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - is an entry point
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k >= 'a'; k--)
+	{
+		putchar(k);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

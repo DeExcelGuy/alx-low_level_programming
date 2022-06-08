9f5736528e2b6156dc007b26a5a8facda253dc37
DeExcelGuy <nreleattobra@gmail.com>
2022-06-07 20:07:14 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - to check for lowercase letters
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}

cabe646b81e449496a87b0b961fdcc6f3f120063
DeExcelGuy <nreleattobra@gmail.com>
2022-06-07 20:28:43 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of integer input
+ * @n: integer to modify
+ *
+ * Return: l
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+	n = -n;
+	_putchar(n + '0');
+	return (n);
+}

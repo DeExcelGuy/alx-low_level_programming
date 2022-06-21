ac3aba6f00eb0375749db196c450b2a52ff83309
DeExcelGuy <nreleattobra@gmail.com>
2022-06-21 01:40:46 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: source string
+ * @accept: accepted string
+ *
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+
+	i = strspn(s, accept);
+
+	return (i);
+}

e03129bb8216c8e7f6928132b668e67acf97d640
DeExcelGuy <nreleattobra@gmail.com>
2022-06-21 17:02:11 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,9 +10,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int a = 0, b, t = 0;
 
-	i = strspn(s, accept);
-
-	return (i);
+	while (accept[a])
+	{
+		b = 0;
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (t);
 }
